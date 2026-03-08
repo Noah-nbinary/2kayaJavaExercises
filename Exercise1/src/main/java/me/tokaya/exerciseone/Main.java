99dbcfabad941e7a987b905009b1becf95b8b899
Noah-nbinary <noah.nbinary@gmail.com>
2026-03-08 17:16:32 +0100

    Solved Exercise 2

--- Exercise1/src/main/java/me/tokaya/exerciseone/Main.java
+++ Exercise1/src/main/java/me/tokaya/exerciseone/Main.java
@@ -104,4 +104,5 @@ public class Main {
         return true;
 
     }
+
 }
--- Exercise2/src/main/java/me/tokaya/exercisetwo/Main.java
+++ Exercise2/src/main/java/me/tokaya/exercisetwo/Main.java
@@ -40,10 +40,146 @@ package me.tokaya.exercisetwo;
  * - Try separating parts of the game into methods if you want extra practice
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    static void main() {
+
+    static String playerName;
+    static int coins = 50;
+    static int bet;
+    static int wins = 0;
+    static int loses = 0;
+    static Scanner scan = new Scanner(System.in);
+
+    static void main() throws Exception {
+
+        gameInitialization();
+
+        while ( coins > 0 ) {
+
+            placeBet();
+
+            rollAndCheckDice();
+
+            keepPlaying();
+
+        }
+
+    }
+
+    static void gameInitialization() {
+        System.out.println("Welcome to the dice betting game!\nInsert your name: ");
+        playerName = scan.nextLine();
+    }
+
+    static void placeBet() {
+
+        System.out.println(
+                "Go on " + playerName + "! Choose how many coins to bet\n" +
+                        "Currently you have " + coins + " coins"
+        );
+
+        while (true) {
+
+            try {
+                bet = Integer.parseInt(scan.nextLine());
+            } catch ( Exception e ) {
+                System.out.println("Please provide a number");
+                continue;
+            }
+
+            if ( bet > coins || bet <= 0 ) {
+                System.out.println(
+                    "Please provide a valid bet, according to your current coins: " + coins
+                );
+            } else {
+                return;
+            }
+        }
+
+    }
+
+    static void rollAndCheckDice() throws Exception {
+
+        int wait = 0;
+
+        System.out.print("Throwing die");
+
+        while ( wait < 3 ) {
+            wait += 1;
+            System.out.print(" .");
+            Thread.sleep(800);
+        }
 
 
+        int diceResult = new Random().nextInt(1,6);
 
+        if ( diceResult >= 4 ) {
+            coins += bet;
+            wins += 1;
+            System.out.println(
+                "\nYou die was " + diceResult + " You won!! \n"+
+                "You now have " + coins + " coins"
+            );
+        }
+
+        if ( diceResult <= 3 ) {
+            coins -= bet;
+            loses += 1;
+            System.out.println(
+                "\nYou die was " + diceResult + " You lost : ( \n"+
+                "You now have " + coins + " coins"
+            );
+        }
+
+    }
+
+    static void keepPlaying() {
+
+        if ( coins <= 0 ) {
+            triggerGameEnd();
+        }
+
+        System.out.println("You want to keep playing? [yY/nN]");
+
+        while (true) {
+
+            String answer;
+
+            try {
+                answer = scan.nextLine().toLowerCase();
+            } catch ( Exception e ) {
+                System.out.println("Please provide [yY/nN] as an answer");
+                continue;
+            }
+
+            if (answer.equals("y") || answer.isEmpty()) {
+                return;
+            }
+
+            if (answer.equals("n")) {
+                triggerGameEnd();
+            }
+
+            System.out.println("Please provide [yY/nN] as an answer");
+
+        }
+    }
+
+    static void triggerGameEnd(){
+
+        System.out.println(
+            "You ended the game with " + coins + "coins\n" +
+            "You won " + wins + " times and lost " + loses + " times"
+        );
+
+        if ( wins > loses && coins > 0 ) {
+            System.out.println("You did amazing!!");
+        } else {
+            System.out.println("Better luck next time pal!");
+        }
+
+        System.exit(0);
     }
 }
