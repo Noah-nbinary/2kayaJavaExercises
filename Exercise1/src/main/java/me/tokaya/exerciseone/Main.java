1bc39aa9e38788fcf013e89c833de5c00b50d904
AriRobles <arizetarb00@gmail.com>
2026-03-08 15:39:14 +0100

    Ejercicio 1 Ari resuelto

--- Exercise1/src/main/java/me/tokaya/exerciseone/Main.java
+++ Exercise1/src/main/java/me/tokaya/exerciseone/Main.java
@@ -35,10 +35,39 @@ package me.tokaya.exerciseone;
  * - Make sure the program does not crash if the user inputs something unexpected
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
 
+        System.out.println("*** JUEGO DE NUMERO SECRETO ***\nIntroduce numeros hasta adivinar el numero secreto");
 
+        //Creamos la constatne de numero secreto
+        Random random = new Random();
+        final int NUMERO_SECRETO = random.nextInt(1, 100);
 
+        //creamos scanner y condicion de victoria
+        Scanner consola = new Scanner(System.in);
+        var ganador = false;
+
+        do{
+            int numeroUsuario = 0;
+            System.out.print("Introduce un numero: ");
+            try{
+            numeroUsuario = Integer.parseInt(consola.nextLine());
+
+            }catch(Exception e){ System.out.println("El valor no es correcto"); }
+
+            if(NUMERO_SECRETO == numeroUsuario) {
+                ganador = true;
+            }else if(numeroUsuario < NUMERO_SECRETO){
+                    System.out.println("El numero secreto es mayor que " + numeroUsuario + " vuelva a intentarlo");
+                } else {
+                    System.out.println("El numero secreto es menor que " + numeroUsuario + " vuelva a intentarlo");
+                }
+        }while(ganador == false);
+
+        System.out.println("FELICIDADES! HAS GANADO! El número secreto era: " +NUMERO_SECRETO);
     }
 }
